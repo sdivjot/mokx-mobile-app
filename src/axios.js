1040fb3b17ea3db65a547625956ea04648701a12
Divjot <singhdivjot10@gmail.com>
2023-08-13 01:07:08 +0530

    api patch

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBaseURL = 'https://20.235.118.112:5000';
+const apiBaseURL = 'https://mokxweb.duckdns.org:5000';
 //https://20.235.118.112:5000/generate
 
 export const api = axios.create({
